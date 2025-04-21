cd4cfaef82144cb60be002bce6aa86e65c04f4b4
leonardolazilha <lazilhaleonardo@gmail.com>
2025-04-20 23:48:11 -0300

    estilizando com StyleSheet

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>meu app</Text>
+      <Image source={require('./pomodoro.png')} style={styles.image}/>
+      <View style={styles.actions}/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#021123',
+    gap: 40
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+  actions: {
+    paddingVertical: 24,
+    paddingHorizontal: 24,
+    backgroundColor: '#14448080',
+    width: '80%',
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: '#144480'
+  }
+})

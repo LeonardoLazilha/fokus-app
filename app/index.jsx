3bfbb95bab94b6352deda33ed964dfd68029792f
leonardolazilha <lazilhaleonardo@gmail.com>
2025-04-21 00:06:25 -0300

    add estilos

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, Image, Pressable } from "react-native";
 
 export default function Index() {
   return (
@@ -6,7 +6,16 @@ export default function Index() {
       style={styles.container}
     >
       <Image source={require('./pomodoro.png')} style={styles.image}/>
-      <View style={styles.actions}/>
+      <View style={styles.actions}>
+      <Text style={styles.timer}>25:00</Text>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Começar</Text>
+      </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Projeto de aprendizado desenvolvido por</Text>
+        <Text style={styles.footerText}>Leonardo Lazilha</Text>
+      </View>
     </View>
   );
 }
@@ -30,6 +39,32 @@ const styles = StyleSheet.create({
     width: '80%',
     borderRadius: 32,
     borderWidth: 2,
-    borderColor: '#144480'
+    borderColor: '#144480',
+    gap: 32
+  },
+  timer: {
+    fontSize: 54,
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  button: {
+    backgroundColor: '#B872FF',
+    borderRadius: 32,
+    padding: 8
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#021123',
+    fontSize: 18,
+    fontWeight: 700
+  },
+  footer: {
+    width: '80%'
+  },
+  footerText: {
+    textAlign: 'center',
+    color: '#98A0A8',
+    fontSize: 12.5
   }
 })
